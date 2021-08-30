f887d6a0960f3552d3875ef9de464aac24e75a39
cuong3004 <65102570+cuong3004@users.noreply.github.com>
2021-08-30 17:32:57 +0700

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ class Block(keras.Model):
                 layers.BatchNormalization(),
                 layers.ReLU(),
                 layers.Conv2D(out_dim_2, (3,3), padding="same"),
-                layers.BatchNormalization()
+                layers.BatchNormalization(),
                 layers.ReLU(),   
             ]
         )
